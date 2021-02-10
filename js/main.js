ec297afd6d78b77c9dd6d36a515a41812e1b2551
YtavPeer <ytav.peer@gmail.com>
2021-02-10 12:06:19 +0200

    1206

--- js/main.js
+++ js/main.js
@@ -5,7 +5,38 @@ var gElCanvas;
 var gCtx;
 
 function init() {
+
       gElCanvas = document.getElementById('my-canvas');
       gCtx = gElCanvas.getContext('2d');
+      renderMeme()
 }
 
+
+function renderMeme() {
+      const currMeme = getGmeme();
+      const currImage = getCurrImg(currMeme.selectedImgId);
+      const img = new Image();
+      img.src = currImage.imgUrl;
+      img.onload = () => {
+            gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+            currMeme.lines.forEach(line => {
+                  drawText(200, 70, line.txt, line.size, line.align, line.color)
+            });
+      }
+}
+
+function drawText(x, y, text, fontSize, align, color) {
+      gCtx.lineWidth = 2
+      gCtx.strokeStyle = color
+      gCtx.fillStyle = 'white'
+      gCtx.font = `${fontSize}px Arial`
+      gCtx.textAlign = align;
+      gCtx.fillText(text, x, y)
+      gCtx.strokeText(text, x, y)
+}
+
+function onAddTextLine() {
+      var newText = document.querySelector('.text-line').value;
+      addTextLine(newText);
+      renderMeme()
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -2,16 +2,42 @@
 
 /* Globals */
 var gKeywords = { 'happy': 12, 'funny puk': 1 };
-var gImgs = [{ id: 1, imgUrl: 'img/popo.jpg', Keywords: ['happy', 'funny'] }];
+
+var gImgs = [{ id: 1, imgUrl: './img-square/1.jpg', Keywords: ['happy', 'funny'] }];
 var gMeme = {
-      selectedImgId: 5,
+      selectedImgId: 1,
       selectedLineIdx: 0,
       lines: [
             {
                   txt: 'I never eat Falafel',
                   size: 20,
                   align: 'left',
-                  color: 'red'
+                  color: 'blue'
             }
       ]
 }
+
+
+function getGmeme() {
+      return gMeme;
+}
+
+
+function getCurrImg(imgId) {
+      return gImgs.find(img => {
+            return img.id === imgId;
+      })
+}
+
+
+function addTextLine(newText) {
+      var newLine = {
+            txt: newText,
+            size: 20,
+            align: 'left',
+            color: 'red'
+      }
+      gMeme.lines.push(newLine);
+}
+
+
