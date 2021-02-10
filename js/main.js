612395c418f487113d459cdc14dfae6520e8e101
YtavPeer <ytav.peer@gmail.com>
2021-02-10 10:59:24 +0200

    commit 11:00

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+var gElCanvas;
+var gCtx;
+
+function init() {
+      gElCanvas = document.getElementById('my-canvas');
+      gCtx = gElCanvas.getContext('2d');
+}
+
---
+++ js/meme-service.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* Globals */
+var gKeywords = { 'happy': 12, 'funny puk': 1 };
+var gImgs = [{ id: 1, imgUrl: 'img/popo.jpg', Keywords: ['happy', 'funny'] }];
+var gMeme = {
+      selectedImgId: 5,
+      selectedLineIdx: 0,
+      lines: [
+            {
+                  txt: 'I never eat Falafel',
+                  size: 20,
+                  align: 'left',
+                  color: 'red'
+            }
+      ]
+}
